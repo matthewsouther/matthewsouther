7063657747c29962a461a35f561e2c58ba8f3e26
Matthew Souther <matthewsouther@gmail.com>
2023-11-02 21:23:52 -0700

    Fix links

--- src/music/music.ts
+++ src/music/music.ts
@@ -1,33 +1,39 @@
-import { LitElement, html, css } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { LitElement, html, css } from "lit";
+import { customElement } from "lit/decorators.js";
 
-@customElement('page-music')
+@customElement("page-music")
 export class Music extends LitElement {
   render() {
     return html`
       <h2>Music</h2>
       <p>
-        Trumpet player and composer who dabbles in piano and vocals. Or is it the
-        other way around?
+        Trumpet player and composer who dabbles in piano and vocals. Or is it
+        the other way around?
       </p>
       <p>Studied music composition at Bennington College.</p>
       <p>Post-college, was a part of:</p>
       <ul>
         <li>
-          <a href="ilgato.com/">il gato</a>
+          <a href="https://ilgato.com/">il gato</a>
         </li>
         <li>
-          <a href="pickpocketensemble.com/">the pickPocket ensemble</a>
+          <a href="https://pickpocketensemble.com/">the pickPocket ensemble</a>
         </li>
       </ul>
       <p>Here are some songs he wrote and recorded circa 2009:</p>
       <h3>The Clams</h3>
       <audio controls>
-        <source src="https://s3.us-west-1.amazonaws.com/www.matthewsouther.com/the_clams.mp3" type="audio/mpeg" />
+        <source
+          src="https://s3.us-west-1.amazonaws.com/www.matthewsouther.com/the_clams.mp3"
+          type="audio/mpeg"
+        />
       </audio>
       <h3>The Woodcock's Flight</h3>
       <audio controls>
-        <source src="https://s3.us-west-1.amazonaws.com/www.matthewsouther.com/the_woodcocks_flight.mp3" type="audio/mpeg" />
+        <source
+          src="https://s3.us-west-1.amazonaws.com/www.matthewsouther.com/the_woodcocks_flight.mp3"
+          type="audio/mpeg"
+        />
       </audio>
       <p>
         Some of his open mic stuff from July 2023 is
@@ -38,6 +44,8 @@ export class Music extends LitElement {
   }
 
   static styles = css`
-    h2 { color: indianred; }
-  `
+    h2 {
+      color: indianred;
+    }
+  `;
 }
